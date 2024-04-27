785fbcb80d6b2a61188b60fe1c4594596c55e773
Akisa-Moruri <akisamoruri@gmail.com>
2024-04-27 16:15:27 +0300

    state abd events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // State variable to keep track of whether the item is in the cart
+  const [inCart, setInCart] = useState(false);
+
+  // Function to handle adding/removing item from cart
+  const handleCartToggle = () => {
+    setInCart(!inCart);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/* Button to add/remove item from cart */}
+      <button className="add" onClick={handleCartToggle}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
